40345cf1ca0c9957d4a96e43077e1cacd84bc939
pooja.bhat <poojabhat1690@gmail.com>
2017-06-22 18:56:25 +0200

    Added the colnames to the output

--- countingWindowsGreaterThan5cpm.R
+++ countingWindowsGreaterThan5cpm.R
@@ -18,25 +18,6 @@ colnames(onlyCounts) = c(colnames(metaData),as.character(sampleInfoPath$V4))
 onlyCounts = onlyCounts[-which(apply(onlyCounts[,c(7:ncol(onlyCounts))],1,max)<5),]
 
 
-
+colnames(onlyCounts) = c("chr","start","end","geneName","score","strand")
 write.table(onlyCounts,"/Volumes/groups/ameres/Pooja/Projects/zebrafishAnnotation/zebrafish_analysis/importantDataframes/countingWindowsGreaterThan5cpm.bed",sep="\t",row.names = F,quote = F)
 
-
-
-### remove injection smaples
-
-
-onlyCounts_minusInjection  = onlyCounts[,-grep("injection",colnames(onlyCounts))]
-
-vox = onlyCounts_minusInjection[grep("vox",onlyCounts_minusInjection$Name),]
-vox = melt(vox[7:length(vox)])
-ggplot(vox,aes(x=variable,y=value)) + geom_point()
-
-nanog = onlyCounts_minusInjection[grep("nanog",onlyCounts_minusInjection$Name),]
-nanog = melt(nanog[7:length(nanog)])
-ggplot(nanog,aes(x=variable,y=value)) + geom_point()
-
-
-sox19b = onlyCounts_minusInjection[grep("sox19b",onlyCounts_minusInjection$Name),][1,]
-sox19b = melt(sox19b[7:length(sox19b)])
-ggplot(sox19b,aes(x=variable,y=value)) + geom_point()
