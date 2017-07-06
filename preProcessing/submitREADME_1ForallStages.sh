c6a6bbf686c442bb7edf798b2ccae283c04f7f00
pooja.bhat <poojabhat1690@gmail.com>
2017-07-06 15:48:17 +0200

    this is a script that serially performs pre-processing and primer site identification for each of the timepoints for which we can create the annotation.

---
+++ preProcessing/submitREADME_1ForallStages.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+#### this script submits the script README_1 (first part of preprocessing) for annotating at each stage for which SLAMseq is performed. 
+
+/groups/ameres/Pooja/Projects/zebrafishAnnotation/dr10_data/quantseq/105min/README_1.sh
+/groups/ameres/Pooja/Projects/zebrafishAnnotation/dr10_data/quantseq/140min/README_1.sh
+/groups/ameres/Pooja/Projects/zebrafishAnnotation/dr10_data/quantseq/170min/README_1.sh
+/groups/ameres/Pooja/Projects/zebrafishAnnotation/dr10_data/quantseq/30min_256cellStage/README_1.sh
+/groups/ameres/Pooja/Projects/zebrafishAnnotation/dr10_data/quantseq/65min_1000CellStage/README_1.sh
+/groups/ameres/Pooja/Projects/zebrafishAnnotation/dr10_data/quantseq/t0_128cellStage/README_1.sh
+
