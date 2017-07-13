1b49ca9785ffb507ab20b783bd23dbf220e1e8c0
pooja.bhat <poojabhat1690@gmail.com>
2017-07-13 16:53:48 +0200

    added the annotation submission for the different datasets that andi gerneated. These data are only quantSeq (no SLAMseq).

--- preProcessing/submitREADME_1ForallStages.sh
+++ preProcessing/submitREADME_1ForallStages.sh
@@ -3,10 +3,22 @@
 
 #### this script submits the script README_1 (first part of preprocessing) for annotating at each stage for which SLAMseq is performed. 
 
-/groups/ameres/Pooja/Projects/zebrafishAnnotation/dr10_data/quantseq/105min/README_1.sh
-/groups/ameres/Pooja/Projects/zebrafishAnnotation/dr10_data/quantseq/140min/README_1.sh
-/groups/ameres/Pooja/Projects/zebrafishAnnotation/dr10_data/quantseq/170min/README_1.sh
-/groups/ameres/Pooja/Projects/zebrafishAnnotation/dr10_data/quantseq/30min_256cellStage/README_1.sh
-/groups/ameres/Pooja/Projects/zebrafishAnnotation/dr10_data/quantseq/65min_1000CellStage/README_1.sh
-/groups/ameres/Pooja/Projects/zebrafishAnnotation/dr10_data/quantseq/t0_128cellStage/README_1.sh
+#/groups/ameres/Pooja/Projects/zebrafishAnnotation/dr10_data/quantseq/105min/README_1.sh
+#/groups/ameres/Pooja/Projects/zebrafishAnnotation/dr10_data/quantseq/140min/README_1.sh
+#/groups/ameres/Pooja/Projects/zebrafishAnnotation/dr10_data/quantseq/170min/README_1.sh
+#/groups/ameres/Pooja/Projects/zebrafishAnnotation/dr10_data/quantseq/30min_256cellStage/README_1.sh
+#/groups/ameres/Pooja/Projects/zebrafishAnnotation/dr10_data/quantseq/65min_1000CellStage/README_1.sh
+#/groups/ameres/Pooja/Projects/zebrafishAnnotation/dr10_data/quantseq/t0_128cellStage/README_1.sh
 
+
+
+/groups/ameres/Pooja/Projects/zebrafishAnnotation/dataFromAndi/for_Pooja/2016_12_QuantSeq_Annotaton/raw_data/1dpf/README_1.sh
+/groups/ameres/Pooja/Projects/zebrafishAnnotation/dataFromAndi/for_Pooja/2016_12_QuantSeq_Annotaton/raw_data/256cell/README_1.sh
+/groups/ameres/Pooja/Projects/zebrafishAnnotation/dataFromAndi/for_Pooja/2016_12_QuantSeq_Annotaton/raw_data/2cell/README_1.sh
+/groups/ameres/Pooja/Projects/zebrafishAnnotation/dataFromAndi/for_Pooja/2016_12_QuantSeq_Annotaton/raw_data/2dpf/README_1.sh
+/groups/ameres/Pooja/Projects/zebrafishAnnotation/dataFromAndi/for_Pooja/2016_12_QuantSeq_Annotaton/raw_data/4dpf/README_1.sh
+/groups/ameres/Pooja/Projects/zebrafishAnnotation/dataFromAndi/for_Pooja/2016_12_QuantSeq_Annotaton/raw_data/bud/README_1.sh
+/groups/ameres/Pooja/Projects/zebrafishAnnotation/dataFromAndi/for_Pooja/2016_12_QuantSeq_Annotaton/raw_data/dome/README_1.sh
+/groups/ameres/Pooja/Projects/zebrafishAnnotation/dataFromAndi/for_Pooja/2016_12_QuantSeq_Annotaton/raw_data/oocyte/README_1.sh
+/groups/ameres/Pooja/Projects/zebrafishAnnotation/dataFromAndi/for_Pooja/2016_12_QuantSeq_Annotaton/raw_data/sphere/README_1.sh
+/groups/ameres/Pooja/Projects/zebrafishAnnotation/dataFromAndi/for_Pooja/2016_12_QuantSeq_Annotaton/raw_data/testis/README_1.sh
