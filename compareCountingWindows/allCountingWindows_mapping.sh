4808485bb689e19f90fdc7c9a790c4a012696586
pooja.bhat <poojabhat1690@gmail.com>
2017-07-31 15:28:24 +0200

    adding script that runs slamdunk on counting windows that are a combination of counting windows for all stages

---
+++ compareCountingWindows/allCountingWindows_mapping.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+
+#$ -t 1-24
+
+#$ -pe smp 6-24
+#$ -l hostname=!compute-6-3&!compute-6-14&!compute-6-18&!compute-6-20&!compute-6-3*&!compute-6-4*&!compute-6-5*
+
+
+########$ -wd /home/<your_UCL_id>/Scratch/output
+
+# 8. Parse parameter file to get variables.
+number=$SGE_TASK_ID
+paramfile=/groups/ameres/Pooja/Projects/zebrafishAnnotation/dr10_data/quantseq/sampleInfoIndex.txt
+ 
+index=`sed -n ${number}p $paramfile | awk '{print $1}'`
+variable1=`sed -n ${number}p $paramfile | awk '{print $2}'`
+USENAME=`sed -n ${number}p $paramfile | awk '{print $1}'`
+####variable2=`sed -n ${number}p $paramfile | awk '{print $3}'`
+##variable3=`sed -n ${number}p $paramfile | awk '{print $4}'`
+
+ml cutadapt
+
+
+
+mkdir -p /clustertmp/pooja/mapping_dr10_12052017/adapterTrimmed/
+OUTDIR=/clustertmp/pooja/mapping_dr10_12052017/adapterTrimmed/
+
+cutadapt -a AGATCGGAAGAGCACACGTCTGAACTCCAGTCACNNNNNNATCTCGTATGCCGTCTTCTGCTTG -o "$OUTDIR"/"$index"_adapterTrimmed.fastq -m 18 --trim-n //groups/ameres/Pooja/Projects/zebrafishAnnotation/dr10_data/quantseq/allData/quantseq/"$index"
+
+
+
+module load python
+pip install --user IntervalTree
+module load joblib
+module load pysam
+module load R/3.2.2
+module load samtools/1.3.1
+
+mkdir /clustertmp/pooja/mapping_dr10_12052017/adapterTrimmed/allStages/
+
+OUTDIR_NEW=/clustertmp/pooja/mapping_dr10_12052017/adapterTrimmed/allStages/
+
+/groups/ameres/bioinformatics/tools/slamdunk-new-1/slamdunk/bin/slamdunk all -r /groups/ameres/bioinformatics/references/danio_rerio/dr10/danRer10.fa -o "$OUTDIR_NEW" -b /groups/ameres/Pooja/Projects/zebrafishAnnotation/zebrafish_analysis/importantDataframes/comparingAnnotationsBetweenStages/allCountingWindows_stagesCombined.bed  -fb /groups/ameres/Pooja/Projects/zebrafishAnnotation/zebrafish_analysis/importantDataframes/comparingAnnotationsBetweenStages/windowsForMultimapper.bed -a 5 -n 100 -t 15 -mq 0 -mi 0.95 -m -l -rl 95 "$OUTDIR"/"$index"_adapterTrimmed.fastq 
