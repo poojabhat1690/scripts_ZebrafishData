f2fe737ea852b125f2a16bc63f0486fccdc0a05a
pooja.bhat <poojabhat1690@gmail.com>
2017-09-13 15:55:51 +0200

    downloads some external data from ebi

---
+++ ExternalData/getDataFromExternalSources.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+### this is the data from the paper : Codon Usage and 3' UTR Length Determine Maternal mRNA Stability in Zebrafish.
+## GEO accession : GEO: GSE71609, riboZero RNAseq
+
+cd /clustertmp/pooja/mashimaTomariData/
+
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/006/SRR2138286/SRR2138286.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/007/SRR2138287/SRR2138287.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/008/SRR2138288/SRR2138288.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/009/SRR2138289/SRR2138289.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/000/SRR2138290/SRR2138290.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/001/SRR2138291/SRR2138291.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/002/SRR2138292/SRR2138292.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/003/SRR2138293/SRR2138293.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/004/SRR2138294/SRR2138294.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR213/005/SRR2138295/SRR2138295.fastq.gz
