51b1059e69a62aa6d3028d40173b0e932469c3c9
pooja.bhat <poojabhat1690@gmail.com>
2017-10-20 16:17:58 +0200

    this changes chr convention form the data andi gave

---
+++ ucscBrowser/RNAseq/changeChrCoordinates_ribo0_polyA.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+### this is a script that converts the .bigwing files to .bedgraph and then canges the chrs from the format of just numbers to chr+ number to be recognizde by UCSC. 
+### the final step will involve convering this back to bigwig and then copying it over to the seqHub. 
+
+
+
